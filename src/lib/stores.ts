3de2da089c44908486950c38da0766d591d0a62f
Ben Lee <Ben.m.lee@outlook.com>
2023-03-04 18:48:53 +1100

    [fix] pb url

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,7 +3,9 @@ import PocketBase from "pocketbase";
 
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase(dev ? "http://localhost:8090" : "https://website.url")
+export const pbUrl = dev ? "http://localhost:8090" : "http://golf.benlee.me"
+
+export const pb = new PocketBase(pbUrl)
 
 export const user = writable(pb.authStore.model)
 
